8cd5f6a98a7736c649916b615c174e07112ced8d
aynrsyah <aynrsyah.15@gmail.com>
2021-09-20 16:00:39 +0700

    initial commit

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Ayu Nuraisyah
+ Saya tingga di daerah Cikaret
+ olah raga yang saya sukai adalah
+ Lompat tali";
+	}
+
+	//--------------------------------------------------------------------
+
+}
